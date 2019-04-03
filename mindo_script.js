c9038cb1657f3d3eb68c9985d6c9346d209a9ee0
Canis <seth.h.catts@gmail.com>
2019-04-03 12:17:28 -0400

    Adding basic event logging

--- mindo_script.js
+++ mindo_script.js
@@ -4,11 +4,11 @@
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+	  var span = document.createElement("SPAN");
+	  var txt = document.createTextNode("\u00D7");
+	  span.className = "close";
+  	  span.appendChild(txt);
+	  myNodelist[i].appendChild(span);
 }
 
 // Click on a close button to hide the current list item
@@ -25,7 +25,7 @@ for (i = 0; i < close.length; i++) {
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
-  	if (ev.target.tagName === 'LI') {
+	if (ev.target.tagName === 'LI') {
     	ev.target.classList.toggle('checked');
 		console.log("| Item marked complete");
   	}
